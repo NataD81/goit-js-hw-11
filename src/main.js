f1100dfd9a1c1dcef92f60c50a34392343a7a5c1
NataD81 <dvoryanchuk81@gmail.com>
2024-05-24 12:46:25 +0300

    go

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { searchImg } from "./js/pixabay-api";
+import { imgCreated } from "./js/render-functions";
+import { imgTemplate } from "./js/render-functions";
+import { imagesGallery } from "./js/render-functions";
+
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
